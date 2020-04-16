ed38014be5e3c1b25a24b7e02d5ab119dc19088c
chaunnt <chaupad@gmail.com>
2020-04-16 17:03:44 +0700

    update icon app ios and android

--- initProject.sh
+++ initProject.sh
@@ -1,4 +1,13 @@
 #bin/sh
+#create project
+#flutter create --org com.my_cool_org project_name
 
+#init assets for project
 mkdir source/helloflutter/assets
 cp -rf Design/Components/ source/helloflutter/assets
+
+#init images / icon for android
+cp -rf Design/Logo/android/ source/helloflutter/android/app/src/main/res
+
+#init images / icon for ios
+cp -rf Design/Logo/ios/Images.xcassets/ source/helloflutter/ios/Runner/Assets.xcassets
